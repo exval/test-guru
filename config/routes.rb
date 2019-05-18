82e6a3eb826b4a48b6ff3c237109d0fb4c398ea3
Exval <ghost00702@gmail.com>
2019-05-19 00:31:30 +0300

    something change

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,4 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :about do
-  	resources :author
-  end
-
-  # resources :author, path: '/about/author'
-  # match '/about/author' => 'author#index', via: [:get, :post]
-  # get 'about/author' => 'about#author'
-  get 'about/author' => 'author#index'
-  root 'about#index'
 end
