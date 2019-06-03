584a2cecf311c2ce8656e0208bc16cfa89fc325a
Exval <ghost00702@gmail.com>
2019-06-03 22:13:44 +0300

    add history

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,6 @@
 class User < ApplicationRecord
 	
-	def history_test_by_level(level)
-		tests.where(level: level)
-	end
-
-	def list_tests
-		
-	end
+  def tests_by_level(level)
+    Test.where(level: level).joins('INNER JOIN user_tests ON tests.id = user_tests.test_id').where(user_tests: {user_id: self.id})
+  end
 end
