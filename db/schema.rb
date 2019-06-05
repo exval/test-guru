5abd6c4fad3be8fa41295aa37c65cd451e7c989b
exval <ghost00702@gmail.com>
2019-06-05 21:55:27 +0300

    qwer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_130612) do
+ActiveRecord::Schema.define(version: 2019_06_03_191059) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body"
@@ -36,6 +36,15 @@ ActiveRecord::Schema.define(version: 2019_05_23_130612) do
     t.integer "level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_tests", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "test_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["test_id"], name: "index_user_tests_on_test_id"
+    t.index ["user_id"], name: "index_user_tests_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
