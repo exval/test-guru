75a0a185011bea769dc86cbe646811ded01788e9
Exval <ghost00702@gmail.com>
2019-06-18 00:39:58 +0300

    begin_task

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -2,13 +2,11 @@ class TestsController < ApplicationController
 
 	def index
 		@tests = Test.all
-
-		render plain: @tests.inspect
 	end
 
   def show
-    @test = Test.find(params[:id])
+    # @test = Test.find(params[:id])
     
-    render plain: @test.inspect
+    # render plain: @test.inspect
   end
 end
